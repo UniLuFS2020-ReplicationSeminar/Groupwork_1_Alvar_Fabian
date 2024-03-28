022acce7fa2e5ba76049ba86b4e4d2e1648d5485
fabian666666 <fabian.kruegel@bluewin.ch>
2024-03-28 18:34:29 +0100

    add summary statistic plots

--- r-files/01_data_import.R
+++ r-files/01_data_import.R
@@ -1,7 +1,5 @@
 #### data orig
 
-##setwd("C:/Users/fabia/OneDrive/Dokumente/Studium/FS24/Data Mining for Political and Social Sciences/practice/practice/Groupwork_1_Alvar_Fabian/data/data.xlsx")
-
 library(readxl)
 data <- read_excel("data/data.xlsx")
 View(data)
--- r-files/04_plots.R
+++ r-files/04_plots.R
@@ -2,7 +2,38 @@
 # Load required libraries
 library(ggplot2)
 
-# Plot the linear model
+
+# 1. Plotting a histogram for both variables
+par(mfrow=c(1,2)) # Set up a 1x2 grid for plotting
+
+# a. Histogram for econ_satisfaction
+hist(data_prep$econ_satisfaction, main="Economic Satisfaction", xlab="Satisfaction", ylab="Frequency", col="skyblue")
+
+# b. Histogram for trust_politicians
+hist(data_prep$trust_politicians, main="Trust in Politicians", xlab="Trust Level", ylab="Frequency", col="lightgreen")
+
+
+# Boxplot 1
+ggplot(data_prep, aes(x = factor(econ_satisfaction), y = trust_politicians)) +
+  geom_boxplot() +
+  labs(x = "Economic Satisfaction", y = "Trust in Politicians") +
+  ggtitle("Boxplot of Trust in Politicians by Economic Satisfaction")
+
+# Boxplot 2
+ggplot(data_prep, aes(x = factor(trust_politicians), y = econ_satisfaction)) +
+  geom_boxplot() +
+  labs(x = "Trust in Politicians", y = "Economic Satisfaction") +
+  ggtitle("Boxplot of Economic Satisfaction by  Trust in Politicians")
+
+# Heatmap
+ggplot(data_prep, aes(x = econ_satisfaction, y = trust_politicians)) +
+  geom_bin2d() +
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(x = "Economic Satisfaction", y = "Trust in Politicians") +
+  ggtitle("Heatmap of Economic Satisfaction vs. Trust in Politicians")
+
+
+# 2. Plot the linear model
 ggplot(data_prep, aes(x = econ_satisfaction, y = trust_politicians)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
