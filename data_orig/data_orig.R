3a91d025b0616b3c76d1e8b1b43e03505d84b939
alvariusthegreat <alvar.stirnimann@stud.unilu.ch>
2024-03-28 12:36:52 +0100

    organize data import

--- data_orig/data_orig.R
+++
@@ -1,8 +0,0 @@
-#### data orig
-
-setwd("C:/Users/fabia/OneDrive/Dokumente/Studium/FS24/Data Mining for Political and Social Sciences/practice/practice/Groupwork_1_Alvar_Fabian/data/data.xlsx")
-library(readxl)
-data <- read_excel("Groupwork_1_Alvar_Fabian/data_orig/data.xlsx")
-View(data)
-
-# code does not work: manually import dataset -> from excel
